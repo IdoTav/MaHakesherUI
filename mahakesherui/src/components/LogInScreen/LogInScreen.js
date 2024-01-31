40cdef498665e3f6f8d974ac11460bedf8e6a7f1
Daniel Cohen Shor <danielcohenshor@gmail.com>
2024-01-31 21:07:52 +0200

    update

--- mahakesherui/src/components/LogInScreen/LogInScreen.js
+++ mahakesherui/src/components/LogInScreen/LogInScreen.js
@@ -1,9 +1,38 @@
 import './LogInScreen.css';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function LogInScreen () {
+function LogInScreen() {
+    const navigate = useNavigate();
+
+    const handleLogInButtonClick = (e) => {
+        e.preventDefault();
+        navigate('/');
+    };
+
+    const handleCreateAccountClick = (e) => {
+        e.preventDefault();
+        navigate('/');
+    };
+
     return (
-        <div>
-            daniel
+        <div id="background">
+            <p id="title1">LogIn</p>
+            <div id="input-container">
+                <input
+                    id="userNameInput"
+                    placeholder="Username"
+                    className="input"
+                />
+                <input
+                    id="passwordInput"
+                    placeholder="Password"
+                    className="input"
+                    type='password'
+                />
+            </div>
+            <p id="createAccount" onClick={handleCreateAccountClick}>Press here to create account</p>
+            <button id="startButton1" onClick={handleLogInButtonClick}>Log In</button>
         </div>
     );
 };
