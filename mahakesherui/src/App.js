baa0d7bafe0ae4f8278e94590d6b0665cca34c47
Ido David Tavron <idodavid.tavron@live.biu.ac.il>
2024-02-02 13:06:26 +0200

    change names

--- mahakesherui/src/App.js
+++ mahakesherui/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from './components/HomeScreen/HomeScreen';
-import LogInScreen from './components/LogInScreen/LogInScreen';
+import LogInAndRegisterScreen from './components/LogInAndRegisterScreen/LogInAndRegisterScreen';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<HomeScreen />}></Route>
-        <Route path='/LogIn' element={<LogInScreen />}></Route>
+        <Route path='/LogIn' element={<LogInAndRegisterScreen />}></Route>
       </Routes>
     </Router>
   );
--- mahakesherui/src/components/LogInAndRegisterScreen/LogInAndRegisterScreen.js
+++ mahakesherui/src/components/LogInAndRegisterScreen/LogInAndRegisterScreen.js
@@ -1,11 +1,11 @@
-import './LogInScreen.css';
+import './LogInAndRegisterScreen.css';
 import { useNavigate } from 'react-router-dom';
-import logInConsts from './LogInConsts';
+import LogInAndRegisterScreenConsts from './LogInAndRegisterScreenConsts';
 import apiFunction from '../../api/api';
 import apiConsts from '../../api/ApiConsts';
 import {useState} from 'react';
 
-function LogInScreen() {
+function LogInAndRegisterScreen() {
     const navigate = useNavigate();
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
@@ -31,15 +31,15 @@ function LogInScreen() {
 
     return (
         <div id="background">
-            <p id="title1">{logInConsts.LogIn}</p>
+            <p id="title1">{LogInAndRegisterScreenConsts.LogIn}</p>
             <div id="input-container">
                 <input value={userName} onChange={(e) => setUserName(e.target.value)} id="userNameInput" placeholder="Username" className="input"/>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} id="passwordInput" placeholder="Password" className="input" type='password'/>
             </div>
-            <p id="createAccount" onClick={handleCreateAccountClick}>{logInConsts.pressHere}</p>
-            <button id="startButton1" onClick={handleLogInButtonClick}>{logInConsts.LogIn}</button>
+            <p id="createAccount" onClick={handleCreateAccountClick}>{LogInAndRegisterScreenConsts.pressHere}</p>
+            <button id="startButton1" onClick={handleLogInButtonClick}>{LogInAndRegisterScreenConsts.LogIn}</button>
         </div>
     );
 };
 
-export default LogInScreen;
+export default LogInAndRegisterScreen;
