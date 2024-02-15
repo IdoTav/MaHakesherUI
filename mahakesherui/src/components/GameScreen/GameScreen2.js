2869258e93db623d9f6ef842fb0dc9f69350c541
Ido David Tavron <idodavid.tavron@live.biu.ac.il>
2024-02-15 19:57:20 +0200

    game screen2 is done!

--- mahakesherui/src/components/GameScreen/GameScreen2.js
+++ mahakesherui/src/components/GameScreen/GameScreen2.js
@@ -3,27 +3,25 @@ import GameScreenConsts from './GameScreenConsts';
 import backArrow from '../../images/back_arrow.png';
 import { useLocation, useNavigate } from 'react-router-dom';
 import man from '../../images/man_pic.png';
+import woman from '../../images/woman_pic.png'
 import { useState, useEffect } from 'react';
+import arrow from '../../images/arrow.png'
 
 function GameScreen2() {
     const userName = useLocation().state.userName;
     const firstFigure = useLocation().state.firstFigure;
     const lastFigure = useLocation().state.lastFigure
     const navigate = useNavigate();
+    const [figureToShow, setFigureToShow] = useState(firstFigure);
     const optionsList = ["Is connected to", "Appeard in a book", "Was in"];
     const connectionList = ["Sarah - Wife", "Issac - Son", "Ishmael - Son", "Daniel - Stupid", "Daniel - Ugly"];
     const [isShowConnectionBlock, setIsShowConnectionBlock] = useState(false);
     const [historyLists, setHistoryList] = useState([firstFigure, lastFigure]);
-    const [dummy, setDummy] = useState(true);
 
     function handleBackButton(e) {
         e.preventDefault();
         navigate(GameScreenConsts.personalPage, { state: { name: userName } })
     }
-
-    useEffect(() => {
-        let x = 7;}
-    ,[historyLists])
 
     function handleClickOnConnectionFigure(e, figure) {
         e.preventDefault();
@@ -31,11 +29,15 @@ function GameScreen2() {
         const copiedArray = [...historyLists];
         copiedArray.splice(historyLists.length - 1, 0, newConnection);
         setHistoryList(copiedArray);
+        setFigureToShow(newConnection);
+        //TO DO: set new connection buttons
+
     }
 
     function handleClickOnConnection(e) {
         e.preventDefault();
         setIsShowConnectionBlock(true);
+        //TO DO: set new people connection list(sarah - wife etc....)
     }
 
     return (
@@ -46,7 +48,7 @@ function GameScreen2() {
             <div className='GenerateContainerForGame'>
                 <span className='GenerateColumn'>
                     <img className='generateImage1' src={man}></img>
-                    <div className='nameOfFigure'>{firstFigure}</div>
+                    <div className='nameOfFigure'>{figureToShow}</div>
                 </span>
                 <span className='optionsContainer' >
                     {optionsList.map((item, index) => (
@@ -55,6 +57,23 @@ function GameScreen2() {
                         </div>))}
                 </span>
             </div>
+            <div className='historyArrowContainer'>
+                    <span className='historySectionContainer'>
+                        <img className='ImageHistory' src={historyLists[0] === 'Daniel'  || historyLists[0] === 'Sarah' ? woman : man} alt='Man' />
+                        <span className='nameHistory'>{historyLists[0]}</span>
+                    </span>
+                    {historyLists.map((item, index) => (
+                        index !== 0 ?
+                        <span key={index}>
+                            <span className='historySectionContainer'>
+                                <img className='ArrowHistory' src={arrow} alt={`Arrow ${index}`} />
+                            </span>
+                            <span className='historySectionContainer'>
+                                <img className='ImageHistory' src={item === 'Daniel' || item === 'Sarah' ? woman : man} alt={`Man ${index}`} />
+                                <span className='nameHistory'>{item}</span> 
+                            </span>
+                        </span> : ""))}
+                </div>
             { isShowConnectionBlock ?
             <div className='connectionBlock'>
                 <div className='connectionContainer'>
