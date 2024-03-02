b320685cb0c393b72062a54e122e1ca6318c0960
Ido Tavron <IdoTavron7@gmail.com>
2024-03-01 22:37:13 -0800

    game screen1 successfully integrated with the server side (my sql server and back end)

--- mahakesherui/src/components/GameScreen/GameScreen.js
+++ mahakesherui/src/components/GameScreen/GameScreen.js
@@ -3,9 +3,12 @@ import GameScreenConsts from "./GameScreenConsts";
 import backArrow from '../../images/back_arrow.png';
 import light from '../../images/light.png'
 import arrow from '../../images/arrow.png';
-import man from '../../images/man_pic.png'
+import man from '../../images/man_pic.png';
+import female from '../../images/woman_pic.png';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import apiFunction from '../../api/api';
+import apiConsts from '../../api/ApiConsts';
 
 function GameScreen() {
     const navigate = useNavigate();
@@ -14,8 +17,38 @@ function GameScreen() {
     const [isHiddenFigure2, setIsHiddenFigure2] = useState('none');
     const [isCircle1, setIsCircle1] = useState(true);
     const [isCircle2, setIsCircle2] = useState(true);
-    const [firstFigure, setFirstFigure] = useState("Ido");
-    const [lastFigure, setLastFigure] = useState("Tal");
+    const [firstFigure, setFirstFigure] = useState('');
+    const [lastFigure, setLastFigure] = useState('');
+    const [roadPersons, setRoadPersons] = useState([]);
+    const [roadConnections, setRoadConnections] = useState([]);
+
+    const getRoad = async () => {
+        const roadResponse = await apiFunction(apiConsts.Get, apiConsts.serverUrl + "Connections/GetPlayRoad?difficultyLevel=10");
+        parseRoad(JSON.parse(roadResponse));
+    }
+
+    function parseRoad(roadResponse) {
+        const { namesAndGenders, connectionList } = Object.entries(roadResponse).reduce(
+            ({ namesAndGenders, connectionList }, [key, value]) => {
+              const [name, number] = key.split('_');
+              const [description, gender] = value.split('$$$');
+          
+              return {
+                namesAndGenders: { ...namesAndGenders, [name]: gender },
+                connectionList: [...connectionList, description],
+              };
+            },
+            { namesAndGenders: {}, connectionList: [] }
+          );
+          setRoadPersons(namesAndGenders);
+          setRoadConnections(connectionList);
+          setFirstFigure([Object.keys(namesAndGenders)[0], namesAndGenders[Object.keys(namesAndGenders)[0]]]);
+          setLastFigure([Object.keys(namesAndGenders)[Object.keys(namesAndGenders).length - 1], namesAndGenders[Object.keys(namesAndGenders)[Object.keys(namesAndGenders).length - 1]]]);
+    }
+
+    useEffect(() => {
+        getRoad();
+    }, [])
 
     function activateImage(e, figure, circle) {
         e.preventDefault();
@@ -25,8 +58,14 @@ function GameScreen() {
 
     function handleContiuneButton(e) {
         e.preventDefault();
-        navigate('/GameScreen', { state: { firstFigure: firstFigure, lastFigure: lastFigure, userName: userName } });
-
+        navigate('/GameScreen', { state:
+             { firstFigure: firstFigure,
+                lastFigure: lastFigure,
+                userName: userName,
+                roadConnections: roadConnections,
+                roadPersons: roadPersons 
+            } 
+        });
     }
 
 
@@ -34,11 +73,11 @@ function GameScreen() {
     return (
         <div id='background'>
             <img id="backArrow" src={backArrow} onClick={() => navigate(GameScreenConsts.personalPage, { state: { name: userName } })}></img>
-            <div className='GenerateContainer'>
+            <div className='GenerateContainner'>
                 <span className='GenerateColumn'>
                     <div className='figure'>Figure 1</div>
-                    {isCircle1 ? <div className='circle'></div> : <img className='generateImage' src={man}></img>}
-                    <div className='nameOfFigure' style={{display : isHiddenFigure1}}>{firstFigure}</div>
+                    {isCircle1 ? <div className='circle'></div> : <img className='generateImage' src={firstFigure[1] === 'male' ? man : female}></img>}
+                    <div className='nameOfFigure' style={{display : isHiddenFigure1}}>{firstFigure[0]}</div>
                     <button onClick={e => activateImage(e, setIsHiddenFigure1, setIsCircle1)} className='generateButton'>Generate</button>
                 </span>
                 <span className='GenerateCoulumn1'>
@@ -46,8 +85,8 @@ function GameScreen() {
                 </span>
                 <span className='GenerateColumn2'>
                     <div className='figure'>Figure 2</div>
-                    {isCircle2 ? <div className='circle'></div> : <img className='generateImage' src={man}></img>}
-                    <div className='nameOfFigure' style={{display : isHiddenFigure2}}>{lastFigure}</div>
+                    {isCircle2 ? <div className='circle'></div> : <img className='generateImage' src={lastFigure[1] === 'male' ? man : female}></img>}
+                    <div className='nameOfFigure' style={{display : isHiddenFigure2}}>{lastFigure[0]}</div>
                     <button onClick={e => activateImage(e, setIsHiddenFigure2, setIsCircle2)} className='generateButton'>Generate</button>
                 </span>
             </div>
